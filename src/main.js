4fed51f47274159d5c4a3303a2c964ff6c315249
damianhunt <dhuntsa@gmail.com>
2023-03-28 21:53:19 +0200

    updated burger button js and some styling and html

--- src/main.js
+++ src/main.js
@@ -244,3 +244,13 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 });
 //console.log("Hello ");
+
+// hamburger
+function toggleMenu() {
+  document.getElementById("primaryNav").classList.toggle("open");
+  document.getElementById("hamburgerBtn").classList.toggle("open");
+
+}
+
+const x = document.getElementById('hamburgerBtn')
+x.onclick = toggleMenu;

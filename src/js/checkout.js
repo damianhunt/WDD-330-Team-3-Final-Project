322405b52e69f8cf4f775b70cb1035aedd2e7ad9
damianhunt <dhuntsa@gmail.com>
2023-04-05 21:32:45 +0200

    checkout

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -5,9 +5,7 @@ import CheckoutProcess from "./checkOutProcess.mjs";
 const mycheckOut = new CheckoutProcess("so-cart", "checkout-summary");
 mycheckOut();
 
-document
-  .querySelector("#zip")
-  .addEventListener("blur", mycheckOut.calculateOrdertotal.bind(mycheckOut));
+document.querySelector("#zip").addEventListener("blur", mycheckOut.calculateOrdertotal.bind(mycheckOut));
 // listening for click on the button
 document.querySelector("#checkoutSubmit").addEventListener("click", (e) => {
   e.preventDefault();
